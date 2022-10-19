160e883cccb3ef252172a6465ece0307c4ad4a25
Ben Xue <benben886644@163.com>
2022-10-19 19:07:56 +0800

    Update train_dccf_idih.sh

--- runs/train_dccf_idih.sh
+++ runs/train_dccf_idih.sh
@@ -12,13 +12,13 @@ CUDA_VISIBLE_DEVICES=0,1 python3 -W ignore -m torch.distributed.launch --nproc_p
     models/fixed256/idih_dccf_HR_hsv1_clamp.py \
     --ngpu 2 \
     --workers 8 \
-    --batch-size 7 \
+    --batch-size 8 \
     --exp-name=v1 \
     --weights=${low_res_weights}
 
 # test
-weights="harmonization_exps/fixed256/idih_dccf_HR_hsv1_clamp/000_v1/checkpoints/049.pth"
-CUDA_VISIBLE_DEVICES=1 python3 scripts/evaluate_upsample_refiner.py dccf_improved_dih256_HR_clamp ${weights} \
+weights="harmonization_exps/fixed256/idih_dccf_HR_hsv1_clamp/000_v1/checkpoints/069.pth"
+CUDA_VISIBLE_DEVICES=0 python3 scripts/evaluate_upsample_refiner.py dccf_improved_dih256_HR_clamp ${weights} \
     --resize-strategy Fixed256 \
     --version hsl_nobb \
     --config-path config_test_HR.yml 
