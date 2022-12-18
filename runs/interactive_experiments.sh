ffe7faf93868b90340915044ba558d38d74d8da8
nafiturgut <ahmetturgut3@gmail.com>
2022-12-18 15:54:35 +0300

    repo update for colab

--- runs/interactive_experiments.sh
+++ runs/interactive_experiments.sh
@@ -1,20 +1,20 @@
 weights='pretrained_models/dccf_idih_hrnet18s_v2p_HR_pretrain.pth'
 
 CUDA_VISIBLE_DEVICES=0 python scripts/predict_interactive.py hrnet18s_v2p_idih256_upsample_hsl_refine_HR ${weights} \
-    --images assets/interactive_images/ \
-    --masks assets/interactive_images/ \
+    --images $HOME/assets/ \
+    --masks $HOME/assets/ \
     --channel L \
-    --results-path harmonization_exps/interactive_results_L/ 
+    --results-path $HOME/results/
 
 CUDA_VISIBLE_DEVICES=0 python scripts/predict_interactive.py hrnet18s_v2p_idih256_upsample_hsl_refine_HR ${weights} \
-    --images assets/interactive_images/ \
-    --masks assets/interactive_images/ \
+    --images  $HOME/assets/ \
+    --masks  $HOME/assets/ \
     --channel S \
-    --results-path harmonization_exps/interactive_results_S/ 
+    --results-path $HOME/results/
 
-CUDA_VISIBLE_DEVICES=0 python scripts/predict_interactive.py hrnet18s_v2p_idih256_upsample_hsl_refine_HR ${weights} \
-    --images assets/interactive_images_H/ \
-    --masks assets/interactive_images_H/ \
-    --channel H \
-    --results-path harmonization_exps/interactive_results_H/ 
+#CUDA_VISIBLE_DEVICES=0 python scripts/predict_interactive.py hrnet18s_v2p_idih256_upsample_hsl_refine_HR ${weights} \
+#    --images assets/interactive_images_H/ \
+#    --masks assets/interactive_images_H/ \
+#    --channel H \
+#    --results-path harmonization_exps/interactive_results_H/
 
